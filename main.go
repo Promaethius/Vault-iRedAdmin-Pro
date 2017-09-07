1436a3c8c23fda52908ab22bf80bfc757d8c4b66
Promaethius <anon.clipsy.42@gmail.com>
2017-09-06 19:15:27 -0700

    Create main.go
    init basic plugin structure

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "os"
+
+    "github.com/hashicorp/vault/helper/pluginutil"
+    "github.com/hashicorp/vault/plugins"
+)
+
+func main() {
+    apiClientMeta := &pluginutil.APIClientMeta{}
+    flags := apiClientMeta.FlagSet()
+    flags.Parse(os.Args)
+
+    plugins.Serve(New().(iRedAdmin), apiClientMeta.GetTLSConfig())
+}
