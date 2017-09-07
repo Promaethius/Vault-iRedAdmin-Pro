f1d03a2c319c636307d1f2d408d394581f6174d3
Promaethius <anon.clipsy.42@gmail.com>
2017-09-07 12:27:49 -0700

    Update main.go
    init basic function structure

--- main.go
+++ main.go
@@ -2,10 +2,46 @@ package main
 
 import (
     "os"
-
+    // Vault Package Dependencies
     "github.com/hashicorp/vault/helper/pluginutil"
     "github.com/hashicorp/vault/plugins"
+    // Go-Resty Dependency
+    "gopkg.in/resty.v0"
 )
+
+// Define iRedAdmin as an interface to functions. Must satisfy user and server structs
+type iRedAdmin interface {
+    // One method to login and store the cookie for iRedAdmin-Pro
+    loginServer()
+    // Three methods to create, destroy, or renew a user within a domain.
+    createUser()
+    destroyUser()
+    renewUser()
+}
+
+type user struct {
+    username, password, domain string
+}
+
+type server struct {
+    url, admin, adminpass string
+}
+
+func (u user, s server) loginServer() {
+    
+}
+
+func (u user, s server) createUser() {
+    
+}
+
+func (u user, s server) destroyUser() {
+    
+}
+
+func (u user, s server) renewUser() {
+    
+}
 
 func main() {
     apiClientMeta := &pluginutil.APIClientMeta{}
