f1d01130a943759071ceecf3ce8417fccdca7313
Promaethius <anon.clipsy.42@gmail.com>
2017-09-08 14:40:30 -0700

    Update main.go
    used some code from gcp-vault plugin

--- main.go
+++ main.go
@@ -6,14 +6,24 @@ import (
     // Vault Package Dependencies
     "github.com/hashicorp/vault/helper/pluginutil"
     "github.com/hashicorp/vault/logical/plugins"
-    // Go-Resty Dependency
-    "gopkg.in/resty.v0"
+    // Plugin
+    iredadmin "github.com/promaethius/Vault-iRedAdmin-Pro/plugin"
 )
 
 func main() {
-    apiClientMeta := &pluginutil.APIClientMeta{}
-    flags := apiClientMeta.FlagSet()
-    flags.Parse(os.Args)
+	apiClientMeta := &pluginutil.APIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
 
-    plugins.Serve(New().(iRedAdmin-Pro), apiClientMeta.GetTLSConfig())
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := pluginutil.VaultPluginTLSProvider(tlsConfig)
+
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: iredadmin.Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
